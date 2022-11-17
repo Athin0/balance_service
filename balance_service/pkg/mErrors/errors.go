a20a9bf4d72083eef4a44afc8603d2b1321d6a28
Arina <Athin0>
2022-11-17 23:27:44 +0300

    Revert "first v"
    This reverts commit f60c9592f488cdc8a7628f2fa94ef6c0d61f2a4c.

--- balance_service/pkg/mErrors/errors.go
+++
@@ -1,9 +0,0 @@
-package mErrors
-
-import "errors"
-
-var (
-	UnknownUserIdError        = errors.New("user_id does not exist")
-	NotEnoughUserBalanceError = errors.New("user_id has not enough balance")
-	DatabaseError             = errors.New("database error")
-)
